fbdc7c82ff749465349f16ce586ddfc8caa6e276
Shiavm Bhatia <vickishivam@live.com>
2023-12-08 22:17:13 +0530

    Sum of number

--- Sum.js
+++ Sum.js
@@ -7,3 +7,9 @@ function sum(a, b) {
 }
 
 console.log(sum(1, 2, 3, 4, 5));
+//----------------using rest operator
+function sum1(...args) {
+  return args.reduce((a, b) => a + b);
+}
+
+console.log(sum1(1, 2, 3, 4, 5, 8));
